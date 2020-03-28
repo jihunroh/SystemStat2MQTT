1b993dbe82c8f3dc903f572704832bd69f1c6907
JiHun Roh <jihunroh@outlook.kr>
2020-03-28 12:09:00 +0900

    qos 수정 및 출력 정수화

--- SystemStat2MQTT.py
+++ SystemStat2MQTT.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import paho.mqtt.client as mqtt
 import psutil
 from shutil import disk_usage
@@ -12,29 +13,29 @@ class Status:
         return
     def cpu(self):
         return {
-            'percent': psutil.cpu_percent()
+            'percent': int(round(psutil.cpu_percent(), 0))
         }
     def memory(self):
         memory = psutil.virtual_memory()
         return {
             'total': memory.total,
             'used': memory.used,
-            'percent': memory.percent
+            'percent': int(round(memory.percent, 0))
         }
     def disk(self):
         disk = disk_usage('/')
         return {
             'total': disk.total,
             'used:': disk.used,
-            'percent': 100 * round(disk.used / disk.total, 1)
+            'percent': int(round(100 * disk.used / disk.total, 0))
         }
 
 status = Status()
 
 client = mqtt.Client()
 client.connect(MQTT_SERVER, 1883)
-client.publish(MQTT_TOPIC_PREFIX + 'cpu', json_dumps(status.cpu()), qos=0, retain=True)
-client.publish(MQTT_TOPIC_PREFIX + 'memory', json_dumps(status.memory()), qos=0, retain=True)
-client.publish(MQTT_TOPIC_PREFIX + 'disk', json_dumps(status.disk()), qos=0, retain=True)
+client.publish(MQTT_TOPIC_PREFIX + 'cpu', json_dumps(status.cpu()), qos=1, retain=True)
+client.publish(MQTT_TOPIC_PREFIX + 'memory', json_dumps(status.memory()), qos=1, retain=True)
+client.publish(MQTT_TOPIC_PREFIX + 'disk', json_dumps(status.disk()), qos=1, retain=True)
 client.disconnect()
 exit(0)
